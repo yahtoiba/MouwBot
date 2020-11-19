0aec2ceac6bfa28a61a32987fc8e3364cac9f2fc
yahtoiba <73934059+yahtoiba@users.noreply.github.com>
2020-11-19 17:45:23 +0700

    Update donasi.js

--- donasi.js
+++ donasi.js
@@ -1,32 +1,32 @@
 exports.donasi = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagram, telegram, youtube, kapanbotaktif, grupch1, grupch2) => {
-	return `🐼{ *MENU DONASI ${BotName}* }🐼
+	return `🐍{ *MENU DONASI ${botXaturnusSleepy}* }🐍
   
   
 TERIMA KASIH
 *${id.split("@s.whatsapp.net")[0]}*
 TELAH MEMBUKA MENU DONASI
-🐼🐼
+🐍🐍
 
 
-🗓*${tampilTanggal}* 🐼
-⏰*${tampilWaktu}* 🐼
+🗓*${tampilTanggal}* 🐍
+⏰*${tampilWaktu}* 🐍
 (Waktu Server)
 
 KALIAN BISA DONASI MENGGUNAKAN
    
-🛡 *PULSA*: 081271699856
-🛡 *OVO*: 081271699856
-🛡 *DANA* : 081271699866
-🛡 *GOPAY* : 081271699856
+🐍 *PULSA*: 087714745440
+🐊 *OVO*: 087714745440
+🐢 *DANA* : 087714745440
+🦎 *GOPAY* : 087714745440
 
-📺*IKLAN* *YOUTUBE REUZ*
+📺*IKLAN* *YOUTUBE Fadhli Ryan F*
 
 
 ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
 FOLLOW INSTAGRAM SAYA
-${instagram}
+${instagram.com/itspapoy}
 
-🐼TELEGRAM : ${telegram}
+🐍TELEGRAM : ${telegram}
 
 `
 }
