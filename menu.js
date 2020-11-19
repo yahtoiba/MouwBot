6375ecc35513afe2a5616398b5408e82279f7f1f
yahtoiba <73934059+yahtoiba@users.noreply.github.com>
2020-11-19 17:29:17 +0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,134 +1,134 @@
 exports.menu = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagram, telegram, youtube, kapanbotaktif, grupch1, grupch2) => {
-	return `🐼*${BotName}*🐼
+	return `🐍*${botXatunursSleepy}*🐍
   
 HALO *${id.split("@s.whatsapp.net")[0]}* 
 APA KABAR? SEMOGA BAIK-BAIK SAJA!
-Sebelum menggunakan bot ini ada baiknya kalian melihat menu *!info* dan *!donasi* 😁
+Sebelum menggunakan bot ini ada baiknya kalian melihat menu *!info* dan *!donasi* mwehhe😁
 
 💵TOLONG BANTU DONASI AGAR BOT INI BISA TERUS BERJALAN💵
 💵GUNAKAN COMMAND !donasi untuk berdonasi💵
 
-📅*${tampilTanggal}* 🔱
-⏰*${tampilWaktu}* 🔱
+📅*${tampilTanggal}* 🐍
+⏰*${tampilWaktu}* 🐍
 (Waktu Server)
 
-🗒KUMPULAN COMMAND ${BotName}:
+🗒KUMPULAN COMMAND ${botXaturnusSleepy}:
  
-🐼*FUN*🐼
+🐍*FUN*🐍
 
-🐼 _*!pantun*_
-_${BotName}_🍄akan mengirimkanmu pantun secara random
+🐍 _*!pantun*_
+_${botXaturnusSleepy}_🍄akan mengirimkanmu pantun secara random
 
-🐼 _*!animepict*_
-_${BotName}_🍄akan mengirimkanmu gambar anime secara random
+🐍 _*!animepict*_
+_${botXaturnusSleepy}_🍄akan mengirimkanmu gambar anime secara random
 
-🐼 _*!sticker*_
-_${BotName}_🍄akan membuatkan sticker dari gambar yang kamu kirimkan
+🐍 _*!sticker*_
+_${botXaturnusSleepy}_🍄akan membuatkan sticker dari gambar yang kamu kirimkan
 
 Pengggunaan : Kirimkan gambarmu dengan caption !sticker
 
-🐼 _*!nulis <teks>*_
-_${BotName}_🍄akan menuliskan teks yang kamu kirimkan
+🐍 _*!nulis <teks>*_
+_${botXaturnusSleepy}_🍄akan menuliskan teks yang kamu kirimkan
 
 Contoh: !nulis pasti nana bangka dadang ko bang jamping jamping 
 
-🐼 _*!quotes*_
-_${BotName}_🍄akan mencarikanmu quotes secara random
+🐍 _*!quotes*_
+_${botXarurnusSleepy}_🍄akan mencarikanmu quotes secara random
 
-🐼 _*!pict <cewek/cowok>*_
-_${BotName}_🍄akan mengirimkanmu gambar cewek/cowok secara random
+🐍 _*!pict <cewek/cowok>*_
+_${botXaturnusSleepy}_🍄akan mengirimkanmu gambar cewek/cowok secara random
 Contoh: !pict cowok
 
-🐼 _*!animepict*_
-_${BotName}_🍄akan mengirimkanmu gambar anime secara random
+🐍 _*!animepict*_
+_${botXaturnusSleepy}_🍄akan mengirimkanmu gambar anime secara random
 
-🐼 _*!say <teks>*_
-_${BotName}_🍄akan mengirimkan kembali teks yang kamu kirimkan
+🐍 _*!say <teks>*_
+_${botXaturnusSleepy}_🍄akan mengirimkan kembali teks yang kamu kirimkan
 Contoh: !say buset BRO LU GANTENG BANGET AWOKWOKWOKWOWKWOK
 
-🐼 _*!lirik*_ <penyanyi-judul lagu>
-_${BotName}_🍄akan mengirimkanmu lirik lagu yang kamu inginkan
+🐍 _*!lirik*_ <penyanyi-judul lagu>
+_${botXaturnusSleepy}_🍄akan mengirimkanmu lirik lagu yang kamu inginkan
 contoh : !lirik Lisa-Gurenge
 
-🐼 _*!alay*_ <teks>
-_${BotName}_🍄akan mengubah teks yang kamu kirimkan menjadi alay
+🐍 _*!alay*_ <teks>
+_${botXaturnusSleepy}_🍄akan mengubah teks yang kamu kirimkan menjadi alay
 contoh : !alay ampun bang jago
 
 
 ☪*ISLAM*☪
 
-🐼 _*!sholat <daerah>*_
-_${BotName}_🍄akan mengirimkan jadwal sholat sesuai dengan daerah yang kamu kirimkan
+🐍 _*!sholat <daerah>*_
+_${botXaturnusSleepy}_🍄akan mengirimkan jadwal sholat sesuai dengan daerah yang kamu kirimkan
 
 Penggunaan 	: !sholat + daerah kamu
 Contoh		: !sholat Surabaya
 
-🐼 _*!quran*_
-_${BotName}_🍄akan mengirimkanmu ayat Al-Quran secara random
+🐍 _*!quran*_
+_${botXaturnusSleepy}_🍄akan mengirimkanmu ayat Al-Quran secara random
 
 📥*DOWNLOADER*📥
 
-🐼 _*!yt <link>*_
-_${BotName}_🍄akan mendownloadkan video youtube sesuai dengan link yang kamu kirimkan.
+🐍 _*!yt <link>*_
+_${botXaturnusSleepy}_🍄akan mendownloadkan video youtube sesuai dengan link yang kamu kirimkan.
 Contoh: !yt https://youtu.be/linkamu
 
-🐼 _*!ytmp3 <link>*_
-_${BotName}_🍄akan mengubah video youtube menjadi audio sesuai dengan link yang kamu kirimkan
+🐍 _*!ytmp3 <link>*_
+_${botXaturnusSleepy}_🍄akan mengubah video youtube menjadi audio sesuai dengan link yang kamu kirimkan
 
 Contoh: !ytmp3 https://youtu.be/linkamu
 
-🐼 _*!ig <link>*_
-_${BotName}_🍄akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
+🐍 _*!ig <link>*_
+_${botXaturnusSleepy}_🍄akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
 
 Contoh: !ig https://instagram.com/linkamu
 
-🐼 _*!fb <link>*_ //ERROR//
-_${BotName}_🍄akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
+🐍 _*!fb <link>*_ //ERROR//
+_${botXaturnusSleepy}_🍄akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
 
 Contoh: !fb https://facebook.com/linkamu
 
-🐼 _*!twt <link>*_
-_${BotName}_🍄akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
+🐍 _*!twt <link>*_
+_${botXaturnusSleepy}_🍄akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
 
 Contoh: !twt https://twitter.com/linkamu
 
 🎭 _*!tiktok <link>*_
 
-_${BotName}_ akan mengirimkanmu video dari link tiktok yang kamu kirimkan
+_${botXaturnusSleepy}_ akan mengirimkanmu video dari link tiktok yang kamu kirimkan
 
 🖥*EDUKASI*🖥
 
-🐼 _*!wikia <yang kamu cari>*_
-_${BotName}_🍄akan mencarikan yang kamu mau di wikipedia
+🐍 _*!wikia <yang kamu cari>*_
+_${botXaturnusSleepy}_🍄akan mencarikan yang kamu mau di wikipedia
 
 Contoh: !wikia Adolf Hitler
 
-🐼 _*!covid*_
-_${BotName}_🍄akan menampilkan data tentang COVID-19 di Indonesia
+🐍 _*!covid*_
+_${botXaturnusSleepy}_🍄akan menampilkan data tentang COVID-19 di Indonesia
 
 🐰*PRIMBON*🐰
 
-🐼 _*!nama <namakamu>*_
-_${BotName}_🍄akan mencarikan apa arti dari nama kamu
+🐍 _*!nama <namakamu>*_
+_${botXaturnusSleepy}_🍄akan mencarikan apa arti dari nama kamu
 
 Contoh : !nama Stalin
 
-🐼 _*!pasangan <namamu & pasangan>*_
-_${BotName}_🍄akan mencarikan kecocokan antara kamu dan pasanganmu
+🐍 _*!pasangan <namamu & pasangan>*_
+_${botXaturnusSleepy}_🍄akan mencarikan kecocokan antara kamu dan pasanganmu
 
 Contoh : !pasangan covad & covid
 
 
 ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
-FOLLOW INSTAGRAM SAYA
+FOLLOW INSTAGRAM SAYA GAMAKSA OGHEY
 ${instagram}
 
 • TELEGRAM : ${telegram}
-• Youtube : R E U Z
-• WA : 081271699856
+• Youtube : Fadhli Ryan F
+• WA : 087714745440
 • Pasang Iklan? Chat Whatsapp.
 
-📺 *IKLAN* : *YOUTUBE REUZ*
+📺 *IKLAN* : *YOUTUBE Fadhli Ryan F*
 
 🏚SELALU GUNAKAN MASKER SAAT KELUAR!🏚
 🏚SEMOGA SEHAT SELALU!🏚
