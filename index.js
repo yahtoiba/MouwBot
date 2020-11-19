9c52340f7638f6556c38bce975f353c45959b954
yahtoiba <73934059+yahtoiba@users.noreply.github.com>
2020-11-19 17:11:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const BotName = 'botXaturnusSleepy☄'; 
+const instagram = 'https://instagram.com/yahtoiba'; 
+const telegram = 'https://t.me/belumadamwehhe'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const youtube = 'https://www.youtube.com/c/yahtoiba mp4';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
